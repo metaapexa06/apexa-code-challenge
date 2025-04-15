126b66424d35adb765548a24e0bd2853dc0841ab
Apexa Meta <apexa.meta@tatvasoft.com>
2025-04-15 15:51:34 +0530

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Apexa Meta on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountryAppTests/CountryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppTests.swift
+//  CountryAppTests
+//
+//  Created by Apexa Meta on 15/04/25.
+//
+
+import Testing
+@testable import CountryApp
+
+struct CountryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
